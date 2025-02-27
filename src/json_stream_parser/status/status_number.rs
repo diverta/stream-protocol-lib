c3d0b9bc3c64fed556b48a58b7a15657a53fe9ef
Artem <artem@diverta.co.jp>
2025-02-27 17:58:31 +0900

    Improved number parsing to be more general (now handling exponent)

--- src/json_stream_parser/status/status_number.rs
+++ src/json_stream_parser/status/status_number.rs
@@ -19,56 +19,45 @@ impl StatusTrait for StatusNumber {
     }
 
     fn add_char(&mut self, c: &u8) -> Result<Option<(Option<Value>, Option<Status>)>, ParseError> {
-        match (&self.match_so_far[..], self.dot_matched, c) {
-            (_, _, b'0'..=b'9') | // Digits are OK anytime
-            ([], false, b'-') | // Negative sign can only be the first character
-            ([b'-', b'0'..=b'9', ..] | [b'0'..=b'9',..], false, b'.') // The dot may only follow at least one digit with optional sign, if not already matched
+        match c {
+            b'0'..=b'9' | b'e' | b'+' | b'-' | b'.'  // Listing all possible number characters, it's fine if the sequence is incorrect - serde will validate it
             => {
-                if c == &b'.' {
-                    self.dot_matched = true;
-                }
                 self.match_so_far.push(*c);
                 return Ok(None);
             }
-            ([_, ..], _, c) // At least one character mapped - anything non number will be considered a return
-            => {
+            _ => {
                 let mut out_vec = Vec::<u8>::new(); // To be swapped with current data, since that one is no longer needed after this return
                 std::mem::swap(&mut self.match_so_far, &mut out_vec);
-                let serde_number = if self.dot_matched{
-                    let number: f64 = String::from_utf8(out_vec)?.parse()?;
-                    Number::from_f64(number)
-                        .ok_or(ParseError::new(format!("Float is invalid : {}", number)))?
-                } else {
-                    let number: i128 = String::from_utf8(out_vec)?.parse()?;
-                    Number::from_i128(number)
-                        .ok_or(ParseError::new(format!("Integer is invalid : {}", number)))?
-                };
-                // Number is a bit special base type in that we don't know whether we are done parsing until we hit an invalid character
-                // This makes it that we might not only be done with the number itself, but also with the parent object or array
-                // if hitting '}' or ']' respectively. So we have to pass the flag to the parent above
-                let mut done_object = false;
-                let mut done_array = false;
-                let mut comma_matched = false;
-                match c {
-                    b'}' => {
-                        done_object = true;
+                match serde_json::from_slice::<Number>(&out_vec) {
+                    Ok(serde_number) => {
+                        // Number is a bit special base type in that we don't know whether we are done parsing until we hit an invalid character
+                        // This makes it that we might not only be done with the number itself, but also with the parent object or array
+                        // if hitting '}' or ']' respectively. So we have to pass the flag to the parent above
+                        let mut done_object = false;
+                        let mut done_array = false;
+                        let mut comma_matched = false;
+                        match c {
+                            b'}' => {
+                                done_object = true;
+                            },
+                            b']' => {
+                                done_array = true;
+                            },
+                            b',' => {
+                                comma_matched = true;
+                            },
+                            _ => {}
+                        }
+                        return Ok(Some((Some(Value::Number(serde_number)), Some(Status::Done(super::StatusDone::new(
+                            done_object,
+                            done_array,
+                            comma_matched
+                        ))))))
                     },
-                    b']' => {
-                        done_array = true;
-                    },
-                    b',' => {
-                        comma_matched = true;
-                    },
-                    _ => {}
+                    Err(err) => {
+                        return Err(ParseError::new(format!("Invalid number : {err}")));
+                    }
                 }
-                return Ok(Some((Some(Value::Number(serde_number)), Some(Status::Done(super::StatusDone::new(
-                    done_object,
-                    done_array,
-                    comma_matched
-                ))))))
-            }
-            _ => {
-                return Err(ParseError::new("Invalid number"))
             }
         }
     }
